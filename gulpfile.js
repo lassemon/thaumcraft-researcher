ebbd7c3995ef1987aea2189056e7d8e191313ec6
lassemon <lassemon.pirinen@gmail.com>
2015-03-08 21:02:04 +0200

    Add basic build structure with npm, gulp, bower. Take bootstrap into use

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,103 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
+    minifycss = require('gulp-csso'),
+    minifyHtml = require('gulp-minify-html'),
     jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
+    useref = require('gulp-useref'),
+    gulpIf = require('gulp-if'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
     del = require('del');
 
-var paths = {
-
-   compress: [
-        './src/scripts/**/*.js'
-    ],
-    compressed: './dist/assets/js'
-};
-
-// gulp-ruby-sass: 1.x
 gulp.task('styles', function() {
-    return sass('./src/styles/main.scss', { style: 'expanded' })
-        .pipe(autoprefixer('last 2 version', 'safari >= 5', 'ie >= 9', 'chrome >= 36', 'opera >= 12.1', 'ios >= 6', 'android >= 4'))
-        .pipe(gulp.dest('./dist/assets/css'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss())
-        .pipe(gulp.dest('./dist/assets/css'))
-        .pipe(notify({ message: 'Styles task complete' }));
+    return gulp.src('./app/styles/main.scss')
+		.pipe(sass({
+			outputStyle: 'nested',
+			includePaths: ['.'],
+			onError: console.error.bind(console, 'Sass error:')
+		}))
+		.pipe(autoprefixer('last 2 version', 'safari >= 5', 'ie >= 9', 'chrome >= 36', 'opera >= 12.1', 'ios >= 6', 'android >= 4'))
+		.pipe(gulp.dest('./.tmp/styles'))
 });
-
-
-gulp.task('scripts', function() {
-  return gulp.src('./src/scripts/**/*.js')
-    .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/assets/js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/assets/js'))
-    .pipe(notify({ message: 'Scripts task complete' }));
-});
-
 
 gulp.task('images', function() {
-  return gulp.src('src/images/**/*')
+  return gulp.src('./app/images/**/*')
     .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('dist/assets/img'))
-    .pipe(notify({ message: 'Images task complete' }));
+    .pipe(gulp.dest('./dist/assets/img'));
 });
 
-gulp.task('compress', function () {
-    gulp.src(paths.compress)
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.compressed));
+gulp.task('html', ['styles'], function () {
+  var assets = useref.assets({searchPath: ['.tmp', 'app', '.']});
+
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', minifycss()))
+    .pipe(assets.restore())
+    .pipe(useref())
+    //.pipe(gulpIf('*.html', minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('clean', function(cb) {
-    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+    del(['./dist/'], cb)
+});
+
+gulp.task('serve', ['styles'], function () {
+  browserSync({
+    notify: false,
+    port: 8080,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+    
+  });
+
+  gulp.watch([
+    './app/*.html',
+    './app/scripts/**/*.js',
+    './app/images/**/*',
+    './.tmp/**/*'
+  ]).on('change', reload);
+
+  gulp.watch('./app/styles/**/*.scss', ['styles']);
+  gulp.watch('./app/**/*.js', ['lint-client']);
+  gulp.watch('./test/**/*.js', ['lint-test']);
+});
+
+gulp.task('lint-client', function() {
+  return gulp.src('./app/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint-test', function() {
+  return gulp.src('./test/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint', function() {
+  gulp.start('lint-test', 'lint-client');
+});
+
+gulp.task('build', function() {
+  gulp.start('styles', 'images');
 });
 
 gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts', 'images');
+  gulp.start('build');
 });
 
-
-gulp.task('watch', function() {
-
-  // Watch .scss files
-  gulp.watch('src/styles/**/*.scss', ['styles']);
-
-  // Watch .js files
-  gulp.watch('src/scripts/**/*.js', ['scripts']);
-
-  // Watch image files
-  gulp.watch('src/images/**/*', ['images']);
-  
-  // Create LiveReload server
-  livereload.listen();
-
-  // Watch any files in dist/, reload on change
-  gulp.watch(['dist/**']).on('change', livereload.changed);
-
+gulp.task('dist', ['default'], function() {
+  gulp.start('html');
 });
--- src/scripts/main.js
+++
@@ -1,2 +0,0 @@
-var test = "Hello World";
-console.log(test);
